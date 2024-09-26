7324beb7e8f12578e67e40086b9e95a7f5c7c6ef
Tanish Jha <tanish.jha19@gmail.com>
2024-09-26 22:13:21 +0530

    Version2

--- practice.c
+++ practice.c
@@ -2,4 +2,5 @@
 int main(){
     printf("Hello world\n");
     printf("Version 1");
+    printf("This has been changed for version 2");
 }
--- practice2.c
+++ practice2.c
@@ -2,4 +2,5 @@
 int main(){
     printf("Hello world\n");
     printf("Version 2");
+     printf("This has been changed for version 2");
 }
